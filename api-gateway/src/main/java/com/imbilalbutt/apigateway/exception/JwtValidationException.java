066910c2a7def23ed97b09a562d21fadd0040a34
Bilal Ahmad Butt <54913211+imbilalbutt@users.noreply.github.com>
2025-11-06 20:54:31 +0100

    Added Validation in API gateway

---
+++ api-gateway/src/main/java/com/imbilalbutt/apigateway/exception/JwtValidationException.java
@@ -0,0 +1,19 @@
+package com.imbilalbutt.apigateway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+public class JwtValidationException {
+    @ExceptionHandler(WebClientResponseException.Unauthorized.class)
+// Mono object is used by the filter chain to tell the Spring that current filter has finished.
+    public Mono<Void> handleUnauthorizedException(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+}
